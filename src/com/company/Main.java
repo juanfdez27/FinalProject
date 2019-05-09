79a67298575b1788d1aab94c53afdd0bf09debcf
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-05-09 08:54:21 -0400

    Rock, Paper, Scissors, SHOOT!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner keyboard = new Scanner(System.in);
+        String rock;
+        String paper;
+        String scissors;
+        String shoot;
+        System.out.println("Let's play rock, paper, scissors!!");
+        System.out.println("Rock...");
+        System.out.println("Paper...");
+        System.out.println("Scissors...");
+        System.out.println("SHOOT!");
+        shoot = keyboard.nextLine();
+        System.out.println(""+shoot+"");
+
+
+
     }
 }
