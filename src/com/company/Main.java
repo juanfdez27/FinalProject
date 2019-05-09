aae87996ea4cc2eb7760db74383fec827e38d41a
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-05-09 08:37:01 -0400

    keyboard introduced

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
