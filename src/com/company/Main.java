06b609635d61cbd872b58e892fb818f46396d590
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-05-16 08:25:07 -0400

    Switch statement for rock paper and scissors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner keyboard = new Scanner(System.in);
         String rock;
         String paper;
         String scissors;
         String shoot;
-        int [] rockPaperScissors;
+        int[] rockPaperScissors;
         System.out.println("Let's play rock, paper, scissors!!");
         int chosenWeapon = keyboard.nextInt();
         rockPaperScissors = new int[chosenWeapon];
@@ -21,44 +21,39 @@ public class Main {
         System.out.println("Scissors...");
         System.out.println("SHOOT!");
         shoot = keyboard.nextLine();
-        System.out.println(""+shoot+"");
-
-        do {
-            if (shoot < pickedNumber) {
-                System.out.println("Nope, too low. Guess again");
-            }
-            if (shoot > pickedNumber) {
-                System.out.println("Nope, too high. Guess again");
-            }
-
-            shoot = keyboard.nextLine();
-
-
-        }while (shoot != pickedNumber);
+        System.out.println("" + shoot + "");
 
 
 
 
-        if (shoot == pickedNumber){
-            System.out.println("CORRECT!!! You guessed it!");
+    }
+
+    public static int[] inputWeapon(int[] weaponArray, Scanner keyboard) {
+        for (int i=0; i = weaponArray.length; i++){
+            System.out.println("Select a weapon");
+            weaponArray[i] = keyboard.nextInt();
+        }
+        return weaponArray;
+    }
+
+    public static String weaponNumber(int chosenWeapon) {
+        switch (chosenWeapon) {
+            case 1:
+                return "Rock";
+            case 2:
+                return "Paper";
+            case 3:
+                return "Scissors";
+            default:
+                return "error";
         }
 
-
-
     }
-    public static int[]inputWeapon (int[] weaponArray, Scanner keyboard){
-           for (int inputWeapon;) {
-               System.out.println("Select a weapon");
-               weaponArray[2] = keyboard.nextInt();
-           }
-           return weaponArray;
-    }
-    public static String printMonth (int month){
-        switch (month) {
-            case 1:
-                return "January";
-            case 2:
-                return "February";
-            case 3:
-                return "March";
+
+   // public static String shoot (int weaponValue ) {
+       // String rock > String scissors;
+       // String rock < String paper;
+       // String scissors > String paper;
+//}
+
 }
