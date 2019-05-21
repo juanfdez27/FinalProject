d5936abf755d004a260033aff94a5dd4fd36c784
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-05-21 08:49:34 -0400

    Major editing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,75 +11,92 @@ public class Main {
         String paper;
         String scissors;
         String score;
-        int[] rockPaperScissors;
         System.out.println("Let's play rock, paper, scissors!!");
-        System.out.println("If rock type 1, If paper type 2, and If scissors type 3");
+        System.out.println("If rock type 0, If paper type 1, and If scissors type 2");
+        String[] listOfWeapons = new String[3];
+        listOfWeapons[0] = "rock";
+        listOfWeapons[1] = "paper";
+        listOfWeapons[2] = "scissors";
         System.out.println("Select your weapon of choice");
         int chosenWeapon = keyboard.nextInt();
-        rockPaperScissors = new int[chosenWeapon];
-        rockPaperScissors = selectedWeapon(rockPaperScissors, keyboard);
-        int weaponArray;
-        weaponArray = keyboard.nextInt();
+        //rockPaperScissors = new String(listOfWeapons);
+       // rockPaperScissors = chosenWeapon(rockPaperScissors, keyboard);
 
 
-
-        do {
-            if (chosenWeapon > chosenWeapon) {
-                System.out.println("I win");
-            }
-            if (chosenWeapon < chosenWeapon) {
-                System.out.println("You win");
-            }
-            if (chosenWeapon == chosenWeapon){
-                System.out.println("It's a tie");
-            }
-
-            weaponArray = keyboard.nextInt();
-
-
-        }while (chosenWeapon != chosenWeapon );
+        String weaponArray;
+        weaponArray = keyboard.nextLine();
 
         System.out.println("Rock...");
         System.out.println("Paper...");
         System.out.println("Scissors...");
         System.out.println("SHOOT!");
-        weaponArray = keyboard.nextInt();
-        System.out.println("" + weaponArray + "");
+
+        // weaponArray = keyboard.nextLine();
+        // System.out.println("" + weaponArray + "");
 
 
+      //  do {
+           // if (weaponArray > chosenWeapon) {
+           //     System.out.println("I win");
+           // }
+          ///  if (weaponArray < chosenWeapon) {
+          //      System.out.println("You win");
+          //  }
+          //  if (weaponArray == chosenWeapon) {
+          //      System.out.println("It's a tie");
+          //  }
 
+          //  weaponArray = keyboard.nextInt();
+
+
+      //  } while (weaponArray != chosenWeapon);
 
 
     }
 
-    public static int[] selectedWeapon(int[] weaponArray, Scanner keyboard) {
-        for (int i=0; i < weaponArray.length; i++){
-            System.out.println("Confirm your weapon");
-            weaponArray[0] = keyboard.nextInt();
-        }
-        return weaponArray;
-    }
 
-    public static String weaponNumber(int rockPaperScissors) {
-        switch (rockPaperScissors) {
-            case 1:
+
+
+
+
+
+
+
+
+
+
+
+             public static String weaponNumber(int rockPaperScissors) {
+              switch (rockPaperScissors) {
+              case 1:
                 return "Rock";
-            case 2:
+                case 2:
                 return "Paper";
-            case 3:
-                return "Scissors";
-            default:
+                case 3:
+                 return "Scissors";
+             default:
                 return "error";
         }
-
     }
 
-    public static String shoot (int weaponValue ) {
+}
+
+
+
+
+
+
+
+       // }
+
+   // }
+
+   // public static String shoot (int weaponValue ) {
         //case 1 > case 3;
         //case 1 < case 2;
        // case 3 > case 2;
-        return shoot(weaponValue);
-}
+        //return shoot(weaponValue);
+//}
     //public static String score
 
-}
+//}
